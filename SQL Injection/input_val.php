ea28427820454fb2c8c6ce6aced06a4f812c2fab
abdelrhmanhassanmohamed <abdelrhmanhassanmohamed@gmail.com>
2023-08-06 21:25:35 +0300

    update

---
+++ SQL Injection/input_val.php
@@ -0,0 +1,27 @@
+<?php
+
+try {
+    $database = new PDO("mysql:host=localhost;dbname=injection", "root", "");
+} catch (PDOException $e) {
+    die($e->getMessage());
+}
+if (isset($_POST['submit'])) {
+    $username = strip_tags($_POST['username']);
+    $password = strip_tags($_POST['password']);
+    $query = $database->prepare("select * from users where username = :username and password = :password");
+    $query->bindParam("username",$username);
+    $query->bindParam("password",$password);
+    $query->execute();
+    if ($query->rowCount() > 0) {
+        $query = $query->fetch(PDO::FETCH_ASSOC);
+        echo '<p>' . $query['username'] . '</p>';
+        echo '<p>' . $query['password'] . '</p>';
+        echo '<p>' . $query['AuthLevel'] . '</p>';
+    }
+}
+?>
+<form method="post">
+    <input type="text" name="username">
+    <input type="text" name="password">
+    <input type="submit" name="submit" value="login">
+</form>
---
+++ SQL Injection/url.php
@@ -0,0 +1,21 @@
+<?php
+
+try {
+    $database = new PDO("mysql:host=localhost;dbname=injection", "root", "");
+} catch (PDOException $e) {
+    die($e->getMessage());
+}
+if (isset($_GET['id'])) {
+    $id = strip_tags($_GET['id']);
+    $id = (int)$_GET['id'];
+    $query = $database->prepare("select * from users where id=:id");
+    $query->bindParam("id",$id);
+    $query->execute();
+    if ($query->rowCount() > 0) {
+        $query = $query->fetch(PDO::FETCH_ASSOC);
+        echo '<p>' . $query['username'] . '</p>';
+        echo '<p>' . $query['password'] . '</p>';
+        echo '<p>' . $query['AuthLevel'] . '</p>';
+    }
+}
+?>
